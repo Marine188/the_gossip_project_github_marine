6a84f1b6ec7f7852a53b66488837c3eb99b97f91
marine.calderon <marine.calderon@gmail.com>
2019-10-28 17:56:54 +0100

    MAJ jusqu'au2.6

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  get 'welcome/:user_entry', to: 'welcome#show'
+  root 'pages#home'
+  get 'pages/team', to: 'pages#team'
+  get 'pages/contact', to: 'pages#contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
